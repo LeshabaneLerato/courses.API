ab83c37ade010476f02a514ec4e09b5693d9d06c
proliner <proliner@DESKTOP-QSJ92T2>
2026-04-01 12:41:20 +0200

    added course class inside Models folder

---
+++ courses/Models/Course.cs
@@ -0,0 +1,10 @@
+﻿namespace CourseApi.Models
+{
+    public class Course
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public int DurationInHours { get; set; }
+    }
+}
